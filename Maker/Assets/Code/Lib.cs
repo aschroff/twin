f038972c3bca5a5c6a605e83a1df731dccee5511
Andi <andreasschroff@MacBook-Pro-von-Andi.fritz.box>
2022-03-23 17:14:06 +0100

    Rework findings

--- Maker/Assets/Code/Lib.cs
+++ Maker/Assets/Code/Lib.cs
@@ -14,6 +14,7 @@ public class Lib : MonoBehaviour
     public Camera cam;
 
     public Material matFinding;
+    private GameObject currentFinding;
 
     void Start()
     {
@@ -37,10 +38,9 @@ public class Lib : MonoBehaviour
 
     public GameObject DrawFinding(Vector3 x1, Vector3 x2, Vector3 x3)
     {
-
-
-
         GameObject finding = new GameObject("FindingDrawn");
+        finding.transform.SetParent(cube.transform);
+
 
         MeshRenderer meshRendererFinding = finding.AddComponent<MeshRenderer>();
         meshRendererFinding.material = matFinding;
@@ -60,6 +60,98 @@ public class Lib : MonoBehaviour
         meshFinding.RecalculateBounds();
         meshFinding.RecalculateNormals();
         return (finding);
+    }
+
+    public void AddToFinding(Vector3 x1, Vector3 x2, Vector3 x3)
+    {
+        MeshFilter FilterCurrentFinding = currentFinding.GetComponent<MeshFilter>();
+        Mesh meshCurrentFinding = FilterCurrentFinding.mesh;
+
+        GameObject finding = new GameObject("FindingDrawnTemp");
+        finding.transform.SetParent(currentFinding.transform);
+
+        MeshRenderer meshRendererFinding = finding.AddComponent<MeshRenderer>();
+        meshRendererFinding.material = matFinding;
+
+        MeshFilter meshFilterFinding = finding.AddComponent<MeshFilter>();
+
+
+        Mesh meshAdd = new Mesh();
+        meshAdd.vertices = new Vector3[] { x1, x2, x3 };
+        meshAdd.triangles = new int[] { 0, 1, 2 };
+
+        meshFilterFinding.mesh = meshAdd;
+        (finding.AddComponent(typeof(MeshCollider)) as MeshCollider).sharedMesh = meshAdd;
+
+        CombineInstance[] combine = new CombineInstance[2];
+
+        combine[0].mesh = meshCurrentFinding;
+        combine[0].transform = FilterCurrentFinding.transform.localToWorldMatrix;
+
+        combine[1].mesh = meshAdd;
+        combine[1].transform = meshFilterFinding.transform.localToWorldMatrix;
+        meshFilterFinding.gameObject.SetActive(false);
+
+
+        Mesh tempMesh = new Mesh();
+        tempMesh.CombineMeshes(combine);
+        FilterCurrentFinding.mesh = tempMesh;
+
+        meshCurrentFinding.RecalculateBounds();
+        meshCurrentFinding.RecalculateNormals();
+
+    }
+
+    public void ProcessFinding(RaycastHit hit, Mesh mesh, bool add)
+    {
+
+        Vector3[] vertices = mesh.vertices;
+        int[] triangles = mesh.triangles;
+        Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
+        Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
+        Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
+
+
+
+        Transform hitTransform = hit.collider.transform;
+        Vector3 p0n = hitTransform.TransformPoint(p0);
+        Vector3 p1n = hitTransform.TransformPoint(p1);
+        Vector3 p2n = hitTransform.TransformPoint(p2);
+
+        if (add)
+        {
+            AddToFinding(p0n, p1n, p2n);
+        }
+        else
+        {
+            currentFinding = DrawFinding(p0n, p1n, p2n);
+        }
+    }
+
+    public void PaintFinding(float p0x, float p0y, float p1x, float p1y, float p2x, float p2y)
+    {
+
+        SkinnedMeshRenderer twin = cube.GetComponent(typeof(SkinnedMeshRenderer)) as SkinnedMeshRenderer;
+
+        Material material;
+
+        if (twin != null)
+        {
+            material = twin.materials[0];
+            if (material != null)
+            {
+                Texture2D skin = (Texture2D)material.mainTexture;
+
+                Texture2D textureFinding = (Texture2D)materialNew.mainTexture;
+
+                int startX = (int)((System.Math.Min(p0x, System.Math.Min(p1x, p2x))) * skin.width - textureFinding.width / 2);
+                int startY = (int)((System.Math.Min(p0y, System.Math.Min(p1y, p2y))) * skin.height - textureFinding.height / 2);
+                Texture2D skinnew = AddFindingTexture(skin, textureFinding, startX, startY);
+                twin.materials[0].mainTexture = skinnew;
+                SaveTexture(skinnew);
+            };
+        }
+
     }
 
     // Update is called once per frame
@@ -108,9 +200,10 @@ public class Lib : MonoBehaviour
 
         }
 
-        else if ((Input.GetMouseButtonDown(2)) | ((Input.touchCount ==1) && (Input.GetTouch(0).tapCount == 2)))
+        else if ((Input.GetMouseButtonDown(2)) | ((Input.touchCount == 1) && (Input.GetTouch(0).tapCount == 2)))
         {
             Debug.Log("Lib - GetMouseButtonDown(2)");
+
 
 
             RaycastHit hit;
@@ -119,6 +212,40 @@ public class Lib : MonoBehaviour
                 return;
             }
 
+            MeshCollider meshCollider = hit.collider as MeshCollider;
+            if (meshCollider == null || meshCollider.sharedMesh == null)
+                return;
+
+
+            Mesh mesh = meshCollider.sharedMesh;
+
+            ProcessFinding(hit, mesh, false);
+
+
+            float p0x = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 0]].x;
+            float p0y = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 0]].y;
+            float p1x = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 1]].x;
+            float p1y = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 1]].y;
+            float p2x = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 2]].x;
+            float p2y = mesh.uv[mesh.triangles[hit.triangleIndex * 3 + 2]].y;
+
+            if (Input.GetKey("b"))
+            {
+                PaintFinding(p0x, p0y, p1x, p1y, p2x, p2y);
+            }
+
+            
+        }
+        else if (Input.GetMouseButton(2))
+        {
+            Debug.Log("Lib - GetMouseButton(2)");
+            Debug.Log(Input.mousePosition);
+
+            RaycastHit hit;
+            if (!Physics.Raycast(cam.ScreenPointToRay(Input.mousePosition), out hit))
+            {
+                return;
+            }
 
             MeshCollider meshCollider = hit.collider as MeshCollider;
             if (meshCollider == null || meshCollider.sharedMesh == null)
@@ -126,58 +253,8 @@ public class Lib : MonoBehaviour
 
 
             Mesh mesh = meshCollider.sharedMesh;
-            Vector3[] vertices = mesh.vertices;
-            int[] triangles = mesh.triangles;
-            Vector3 p0 = vertices[triangles[hit.triangleIndex * 3 + 0]];
-            Vector3 p1 = vertices[triangles[hit.triangleIndex * 3 + 1]];
-            Vector3 p2 = vertices[triangles[hit.triangleIndex * 3 + 2]];
 
-
-
-            Transform hitTransform = hit.collider.transform;
-            Vector3 p0n = hitTransform.TransformPoint(p0);
-            Vector3 p1n = hitTransform.TransformPoint(p1);
-            Vector3 p2n = hitTransform.TransformPoint(p2);
-
-
-            DrawFinding(p0n, p1n, p2n);
-
-            Debug.DrawLine(p0n, p1n, Color.red, 100.0f);
-            Debug.DrawLine(p1n, p2n, Color.red, 100.0f);
-            Debug.DrawLine(p2n, p0n, Color.red, 100.0f);
-
-            float p0x = mesh.uv[triangles[hit.triangleIndex * 3 + 0]].x;
-            float p0y = mesh.uv[triangles[hit.triangleIndex * 3 + 0]].y;
-            float p1x = mesh.uv[triangles[hit.triangleIndex * 3 + 1]].x;
-            float p1y = mesh.uv[triangles[hit.triangleIndex * 3 + 1]].y;
-            float p2x = mesh.uv[triangles[hit.triangleIndex * 3 + 2]].x;
-            float p2y = mesh.uv[triangles[hit.triangleIndex * 3 + 2]].y;
-
-
-
-            SkinnedMeshRenderer twin = cube.GetComponent(typeof(SkinnedMeshRenderer)) as SkinnedMeshRenderer;
-
-            Material material;
-
-            if (twin != null)
-            {
-                material = twin.materials[0];
-                if (material != null)
-                {
-                    Texture2D skin = (Texture2D)material.mainTexture;
-
-                    Texture2D textureFinding = (Texture2D)materialNew.mainTexture;
-
-                    int startX = (int)((System.Math.Min(p0x, System.Math.Min(p1x, p2x))) * skin.width - textureFinding.width / 2);
-                    int startY = (int)((System.Math.Min(p0y, System.Math.Min(p1y, p2y))) * skin.height - textureFinding.height / 2);
-                    Texture2D skinnew = AddFindingTexture(skin, textureFinding, startX, startY);
-                    twin.materials[0].mainTexture = skinnew;
-                    SaveTexture(skinnew);
-                };
-            }
-
-
-
+            ProcessFinding(hit, mesh, true);
         }
     }
     public static Texture2D AddFindingTexture(Texture2D skin, Texture2D finding, int startPositionX, int startPositionY)
