ac7844eae981942db3a5e2f8b4b07f31bd1e748b
andreasschroff <andreas@schroff-online.com>
2023-06-07 20:22:45 +0200

    test commit

--- Maker/Assets/Code/Singleton.cs
+++ Maker/Assets/Code/Singleton.cs
@@ -27,6 +27,9 @@ public class Singleton<T> : MonoBehaviour where T : Singleton<T>
             Instance = (T)this;
     }
 
+    /// <summary>
+    ///     Destroy method
+    /// </summary>
     protected virtual void OnDestroy()
     {
         if (Instance == this)
