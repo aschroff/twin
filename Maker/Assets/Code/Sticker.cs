93ea1253e2ef3dd1fb4b0dd41dce09d7b8f1ed92
andreasschroff <andreas@schroff-online.com>
2023-07-20 10:54:27 +0200

    Save works untested

--- Maker/Assets/Code/Sticker.cs
+++ Maker/Assets/Code/Sticker.cs
@@ -2,24 +2,51 @@ using UnityEngine;
 using UnityEngine.UI;
 using PaintIn3D;
 using CW.Common;
+using System.IO;
 
 public class Sticker : MonoBehaviour
 
 {
 	[SerializeField] private Texture2D loadedTexture;
 
-    [System.Obsolete]
+	//[System.Obsolete]
     public void SelectAndUseNewTexture()
     {
         Debug.Log("SelectAndUseNewTexture " + this.name);
 		SelectTexture(1024);
-		GameObject icon = this.transform.FindChild("Icon").gameObject;
-		Image iconImage = icon.GetComponent<Image>();
-        iconImage.sprite = Sprite.Create(loadedTexture,new Rect(0, 0, 512, 512), new Vector2());
-		CwDemoButton button = gameObject.GetComponent<CwDemoButton>();
-		GameObject tool = button.IsolateTarget.gameObject;
-		P3dPaintDecal sticker = tool.GetComponent<P3dPaintDecal>();
-		sticker.Texture = loadedTexture;
+		//setTexture();
+
+	}
+
+
+	private void setTexture()
+	{
+		if (loadedTexture != null)
+		{
+			GameObject icon = this.transform.Find("Icon").gameObject;
+			Image iconImage = icon.GetComponent<Image>();
+			Sprite sprite = Sprite.Create(loadedTexture, new Rect(0, 0, 512, 512), new Vector2());
+			iconImage.sprite = sprite;
+			CwDemoButton button = gameObject.GetComponent<CwDemoButton>();
+			GameObject tool = button.IsolateTarget.gameObject;
+			P3dPaintDecal sticker = tool.GetComponent<P3dPaintDecal>();
+			sticker.Texture = loadedTexture;
+		}
+	}
+
+	private void Start()
+	{
+		string fullPath = Path.Combine(Application.persistentDataPath, this.gameObject.GetComponent<Item>().getId() + ".png");
+
+		if (File.Exists(fullPath))
+		{
+			byte[] imageBytes = File.ReadAllBytes(fullPath);
+			loadedTexture = new Texture2D(1024, 1024);
+			loadedTexture.LoadImage(imageBytes);
+
+			setTexture();
+		}
+		
 
 	}
 
@@ -31,19 +58,39 @@ public class Sticker : MonoBehaviour
 			if (path != null)
 			{
 				// Create Texture from selected image
-				Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize);
+				Texture2D texture = NativeGallery.LoadImageAtPath(path, maxSize, false);
 				if (texture == null)
 				{
 					Debug.Log("Couldn't load texture from " + path);
 					return;
 				}
-				loadedTexture = texture;
+				else
+				{
+					loadedTexture = texture;
+					SaveTexture();
+					setTexture();
+
+				}
 			}
 		});
 
 		Debug.Log("Permission result: " + permission);
 	}
 
+	private void SaveTexture()
+
+
+	{
+
+		string fullPath = Path.Combine(Application.persistentDataPath, this.gameObject.GetComponent<Item>().getId() + ".png");
+		byte[] textureBytes = loadedTexture.EncodeToPNG();
+		File.WriteAllBytes(fullPath, textureBytes);
+		NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(
+			fullPath, "GalleryMaker", Path.GetFileName(fullPath), (success, path) => Debug.Log("Media save! result: " + success + " " + path)
+			);
+		Debug.Log("Permission result: " + permission);
+	}
 
 
 }
+
