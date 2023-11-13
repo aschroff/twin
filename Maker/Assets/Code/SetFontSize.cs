5c23d1ca2433063bd935ab9ad43e31ae8fb6ec8a
andreasschroff <andreas@schroff-online.com>
2023-11-13 18:01:18 +0100

    moved and correct text size logic

--- Maker/Assets/Code/SetFontSize.cs
+++ Maker/Assets/Code/SetFontSize.cs
@@ -2,18 +2,42 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using PaintIn3D;
 
 public class SetFontSize : MonoBehaviour
 {
 
-    [SerializeField] private GameObject gameObjectText;
-    [SerializeField] private int multiplicator;
+    [SerializeField] private GameObject folderTexttools;
+    [SerializeField] private float multiplicator;
 
+    public bool IsInstanceOfPrefabWithName(GameObject obj, string name)
+    {
+        TwinPrefab twinPrefab = obj.GetComponent<TwinPrefab>();
+        if (twinPrefab != null)
+        {
+            return twinPrefab.IsInstanceOfPrefabWithName(name);
+        }
+        return false;
+    }
 
-    
     public void  SetFontSizeOFText(System.Int32 index)
     {
-        gameObjectText.GetComponent<Text>().fontSize = index*multiplicator; 
+
+
+        for (int j = 0; j < folderTexttools.transform.childCount; j++)
+        {
+
+            GameObject child = folderTexttools.transform.GetChild(j).gameObject;
+            TwinPrefab twinPrefab = child.GetComponent<TwinPrefab>();
+            if (IsInstanceOfPrefabWithName(child, "Tool Paint Dynamic Decal"))
+            {
+                P3dPaintDecal textPrefab = child.GetComponent<P3dPaintDecal>();
+                textPrefab.Radius = (index + 1) * multiplicator;
+                Debug.Log("Set font");
+
+            }
+
+        } 
     }
 
     
