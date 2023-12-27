c82a15af38ef65b0504f03ab53aa090e712f4832
andreasschroff <andreas@schroff-online.com>
2023-12-27 11:33:44 +0100

    before new version after OS upgrade

---
+++ Maker/Assets/Code/FileManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using UnityEngine.UI;
+
+public class FileManager : MonoBehaviour
+{
+    [SerializeField] public DataPersistenceManager dataManager;
+    private Dictionary<string> profiles = new Dictionary<string>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
