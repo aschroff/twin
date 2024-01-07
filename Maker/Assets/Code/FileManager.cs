72e79f1e4916f78b60fd80ae2f3ac97f3b6cf6ef
andreasschroff <andreas@schroff-online.com>
2024-01-07 11:31:53 +0100

    corrected prefab for groups and started coding for filemanager

--- Maker/Assets/Code/FileManager.cs
+++ Maker/Assets/Code/FileManager.cs
@@ -6,18 +6,33 @@ using UnityEngine.UI;
 
 public class FileManager : MonoBehaviour
 {
+    [SerializeField] public GameObject prefab;
     [SerializeField] public DataPersistenceManager dataManager;
     private Dictionary<string, string> profiles = new Dictionary<string, string>();
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        foreach (KeyValuePair<string, ConfigData> entry in dataManager.GetAllProfilesGameData())
+        {
+            createConfigEntry(entry);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (this.isActiveAndEnabled == true)
+        {
+            Debug.Log("__");
+        } 
+    }
+    
+    public ConfigData createConfigEntry(KeyValuePair<string, ConfigData> entry)
+    {
+        GameObject configData = Instantiate(prefab);
+        configData.transform.SetParent(this.transform, false);
+        configData.transform.localScale = prefab.transform.localScale;
+        return entry.Value;
     }
 }
