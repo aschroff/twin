83e38407f59003fb6c1440423b49dae7ffc8fecf
aschroff <40732079+aschroff@users.noreply.github.com>
2024-03-30 18:05:11 +0100

    added view parameters to config data and did loadsave for body (#61)

--- Maker/Assets/Code/Body.cs
+++ Maker/Assets/Code/Body.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using PaintIn3D;
 using UnityEngine;
-
+using Lean.Common;
+using Lean.Touch;
 public class Body : MonoBehaviour,ItemFile, IDataPersistence
 {
+   [SerializeField] public GameObject mainCamera;
    public  void handleChange(string profile)
    {
       P3dPaintableTexture texture = this.gameObject.transform.GetComponent<P3dPaintableTexture>();
@@ -38,11 +40,21 @@ public class Body : MonoBehaviour,ItemFile, IDataPersistence
    
    public void LoadData(ConfigData data)
    {
-     
+      LeanPitchYaw control = this.GetComponent<LeanPitchYaw>();
+      control.Yaw = data.yaw;
+      control.Pitch =  data.pitch;
+      mainCamera.transform.position = data.positionCamera;
+      LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+      camera.Zoom = data.sizeCamera;
    }
 
    public void SaveData(ConfigData data)
    {
-      
+      LeanPitchYaw control = this.GetComponent<LeanPitchYaw>();
+      data.yaw = control.Yaw;
+      data.pitch = control.Pitch;
+      data.positionCamera = mainCamera.transform.position;
+      LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+      data.sizeCamera = camera.Zoom;
    }
 }
--- Maker/Assets/Code/DataPersistence/Data/ConfigData.cs
+++ Maker/Assets/Code/DataPersistence/Data/ConfigData.cs
@@ -15,6 +15,10 @@ public class ConfigData
     public AttributesData sampleAttributesData;
     public string commandDetails;
     public string views;
+    public float yaw;
+    public float pitch;
+    public Vector3 positionCamera;
+    public float sizeCamera;
 
     public ConfigData() 
     {
@@ -27,5 +31,9 @@ public class ConfigData
         sampleAttributesData = new AttributesData();
         commandDetails = "";
         views = "";
+        yaw = 0.0f;
+        pitch= 0.0f;
+        positionCamera = new Vector3(x: 0.0f, y: 0.5f, z: 1.0f);
+        sizeCamera = 2.0f;
     }
 }
