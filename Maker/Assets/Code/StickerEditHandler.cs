e8d237bfc4823ac07575683e8818601e1f977096
ella <118105831+ellaa2602@users.noreply.github.com>
2024-04-03 16:12:13 +0200

    handling that resize, flip and turn changes are only applied to active sticker (#65)
    Co-authored-by: Ella Maieutec <ellamaieutec@Air-von-Ella.fritz.box>

---
+++ Maker/Assets/Code/StickerEditHandler.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using PaintIn3D;
+
+//component to find active sticker and apply flip/turn/resize 
+public class StickerEditHandler : MonoBehaviour
+{
+    [SerializeField] private GameObject folderTools;
+
+    public bool IsInstanceOfPrefabWithName(GameObject obj, string name)
+    {
+        TwinPrefab twinPrefab = obj.GetComponent<TwinPrefab>(); //Fehler twinPrefab = null
+        if (twinPrefab != null)
+        {
+            return twinPrefab.IsInstanceOfPrefabWithName(name);
+        }
+        return false;
+    }
+
+    public GameObject GetActiveSticker()
+    {
+
+        for (int j = 0; j < folderTools.transform.childCount; j++)
+        {
+
+            GameObject child = folderTools.transform.GetChild(j).gameObject;
+            TwinPrefab twinPrefab = child.GetComponent<TwinPrefab>();
+            if (IsInstanceOfPrefabWithName(child, "Tool Sticker") && child.activeInHierarchy)
+            {
+                Debug.Log("Found active Sticker");
+                return child;
+
+            }
+
+        }
+        Debug.Log("No active Sticker found");  
+        return null; 
+    }
+
+    public void FlipStickerHorizontal()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if(activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.FlipHorizontal();
+            Debug.Log("Sticker flipped horizontally"); 
+
+        }
+    }
+
+
+    public void FlipStickerVertical()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if (activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.FlipVertical();
+            Debug.Log("Sticker flipped vertically");
+
+        }
+    }
+
+
+    public void TurnStickerToTheLeft()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if (activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.IncrementAngle(45);
+            Debug.Log("Sticker turned to the left");
+
+        }
+    }
+
+    public void TurnStickerToTheRight()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if (activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.IncrementAngle(-45);
+            Debug.Log("Sticker turned to the right");
+
+        }
+    }
+
+    public void ExpandSticker()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if (activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.MultiplyScale(1.5F);
+            Debug.Log("Sticker expanded");
+
+        }
+    }
+
+    public void ShrinkSticker()
+    {
+        GameObject activeSticker = this.GetActiveSticker();
+        if (activeSticker != null)
+        {
+
+            P3dPaintDecal stickerPrefab = activeSticker.GetComponent<P3dPaintDecal>();
+            stickerPrefab.MultiplyScale(0.6667F);
+            Debug.Log("Sticker shrunk");
+
+        }
+    }
+
+}
