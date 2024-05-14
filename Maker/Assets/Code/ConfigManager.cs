4380f262d0fc2ca8298f6518516037b0a21d91cd
Torben Maieutec <torbenmaieutec@Air-von-Torben.fritz.box>
2024-05-14 16:40:09 +0200

    created acces to class TwinNameValidator for first Tests

--- Maker/Assets/Code/ConfigManager.cs
+++ Maker/Assets/Code/ConfigManager.cs
@@ -9,11 +9,17 @@ public class ConfigManager : MonoBehaviour
     [SerializeField] private DataPersistenceManager dataPersistenceManager;
     [SerializeField] private FileManager fileManager;
     [SerializeField] private GameObject newNameOrigin;
+    private TwinNameValidator nameValidator;
     public void newConfig()
     {
         Text origin = newNameOrigin.GetComponent<Text>();
+
+        GameObject NewTwin = this.transform.parent.Find("New").gameObject;
+        nameValidator = NewTwin.GetComponentInChildren<TwinNameValidator>();
+     
         dataPersistenceManager.createNewConfig(origin.text);
         origin.transform.parent.gameObject.GetComponent<InputField>().text = "TwinName";
+
         //textbox should be cleared
         dataPersistenceManager.SaveConfig();
         fileManager.Refresh();
