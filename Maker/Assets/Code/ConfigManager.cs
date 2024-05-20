2ac14278391303ee7caeca02eddaa48217375b8c
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.fritz.box>
2024-05-20 19:30:16 +0200

    name of twin should now be checked if button is pressed

--- Maker/Assets/Code/ConfigManager.cs
+++ Maker/Assets/Code/ConfigManager.cs
@@ -8,16 +8,30 @@ public class ConfigManager : MonoBehaviour
 
     [SerializeField] private DataPersistenceManager dataPersistenceManager;
     [SerializeField] private FileManager fileManager;
-    [SerializeField] private GameObject newNameOrigin;
+
+    [SerializeField] private GameObject inputField;
+
+
     private TwinNameValidator nameValidator;
     public void newConfig()
     {
-        Text origin = newNameOrigin.GetComponent<Text>();
+        //I want the children "Text" from the GameObject inputField
+        // in this children Text I want the component Text to get acces to the origin of the name of the new twin
+        //Text origin = this.transform.GetComponentInChildren()<Text>();
+        Transform Text = inputField.transform.Find("Text");
+        Text origin = Text.GetComponent<Text>();
+        //Text origin = this.transform.Find("Text");
+        Debug.Log("Text from Input: " + origin.text.ToString() + ".");
+
+        // Here I want to acces the the component from the input field
+        // this component twinNameValidator 
+        nameValidator = inputField.GetComponent<TwinNameValidator>();
+
         // getting the name of the new created Twin
 
-        GameObject NewTwin = this.transform.parent.GetComponentInChildren<TwinNameValidator>().gameObject;
+        //GameObject NewTwin = this.transform.parent.GetComponentInChildren<TwinNameValidator>().gameObject;
         //GameObject NewTwin = this.transform.parent.Find("New").gameObject;
-        nameValidator = NewTwin.GetComponentInChildren<TwinNameValidator>();
+        //nameValidator = NewTwin.GetComponentInChildren<TwinNameValidator>();
         // getting Component (TwinNameValidator)of Input Field to call CheckInput-Function
 
         if (nameValidator.CheckInput(origin.text.ToString())) {
@@ -33,7 +47,7 @@ public class ConfigManager : MonoBehaviour
     
     public void saveAsConfig()
     {
-        Text origin = newNameOrigin.GetComponent<Text>();
+        Text origin = inputField.GetComponent<Text>();
         dataPersistenceManager.saveAsConfig(origin.text);
         origin.transform.parent.gameObject.GetComponent<InputField>().text = "TwinName";
         //textbox should be cleared
