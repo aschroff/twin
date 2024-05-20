9562b6cbce21932e149f05b2834881d50045514c
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.fritz.box>
2024-05-20 19:48:27 +0200

    refactored the code in 2 methods, new code currently only working while creating a new twin

--- Maker/Assets/Code/ConfigManager.cs
+++ Maker/Assets/Code/ConfigManager.cs
@@ -8,35 +8,14 @@ public class ConfigManager : MonoBehaviour
 
     [SerializeField] private DataPersistenceManager dataPersistenceManager;
     [SerializeField] private FileManager fileManager;
-
     [SerializeField] private GameObject inputField;
 
-
-    private TwinNameValidator nameValidator;
     public void newConfig()
     {
-        //I want the children "Text" from the GameObject inputField
-        // in this children Text I want the component Text to get acces to the origin of the name of the new twin
-        //Text origin = this.transform.GetComponentInChildren()<Text>();
-        Transform Text = inputField.transform.Find("Text");
-        Text origin = Text.GetComponent<Text>();
-        //Text origin = this.transform.Find("Text");
-        Debug.Log("Text from Input: " + origin.text.ToString() + ".");
-
-        // Here I want to acces the the component from the input field
-        // this component twinNameValidator 
-        nameValidator = inputField.GetComponent<TwinNameValidator>();
-
-        // getting the name of the new created Twin
-
-        //GameObject NewTwin = this.transform.parent.GetComponentInChildren<TwinNameValidator>().gameObject;
-        //GameObject NewTwin = this.transform.parent.Find("New").gameObject;
-        //nameValidator = NewTwin.GetComponentInChildren<TwinNameValidator>();
-        // getting Component (TwinNameValidator)of Input Field to call CheckInput-Function
-
-        if (nameValidator.CheckInput(origin.text.ToString())) {
-            dataPersistenceManager.createNewConfig(origin.text);
-            origin.transform.parent.gameObject.GetComponent<InputField>().text = "TwinName";
+        string newTwinNameFromInput = GetTwinNameFromInput();
+        if (CheckInputTwinName(newTwinNameFromInput)) {
+            dataPersistenceManager.createNewConfig(newTwinNameFromInput);
+            inputField.transform.Find("Text").GetComponent<Text>().text = "TwinName";
             //textbox should be cleared
             dataPersistenceManager.SaveConfig();
             fileManager.Refresh();
@@ -61,6 +40,21 @@ public class ConfigManager : MonoBehaviour
         dataPersistenceManager.ResetApp();
         fileManager.Refresh();
         InteractionController.EnableMode("Main");
+    }
+
+    private string GetTwinNameFromInput() {
+        Transform Text = inputField.transform.Find("Text");
+        Text origin = Text.GetComponent<Text>();
+        Debug.Log("Text from Input: " + origin.text.ToString() + ".");//delete after Testing
+
+        return origin.text.ToString();
+    }
+
+    private bool CheckInputTwinName(string inputForTwinName) {
+        // Here I want to acces the the component from the input field
+        // this component twinNameValidator 
+        TwinNameValidator nameValidator = inputField.GetComponent<TwinNameValidator>();
+        return nameValidator.CheckInput(inputForTwinName);
     }
 
 }
