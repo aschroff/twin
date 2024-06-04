c7147a115fced7dae8387a488e5036b184b3ec9b
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2024-06-04 12:42:12 +0200

    Refactored class commented

--- Maker/Assets/Code/ConfigManager.cs
+++ Maker/Assets/Code/ConfigManager.cs
@@ -10,37 +10,46 @@ public class ConfigManager : MonoBehaviour
     [SerializeField] private FileManager fileManager;
     [SerializeField] private GameObject inputField;
 
+    /*
+     * This methode is responsible, if the button newConfig is pressed in Twin Mode.
+     * This Button creates a complete new Twin
+     */
     public void newConfig()
     {
         string newTwinNameFromInput = GetTwinNameFromInput();
-        if (CheckInputTwinName(newTwinNameFromInput))
-        {
-            dataPersistenceManager.createNewConfig(newTwinNameFromInput);
-            inputField.transform.Find("Text").GetComponent<Text>().text = "TwinName";
-            //textbox should be resetted
+        // getting newest input from the InputField
+        if (CheckInputTwinName(newTwinNameFromInput)) { 
+            // check if the new input is a already existing folder
+            dataPersistenceManager.createNewConfig(newTwinNameFromInput);      
+            inputField.transform.GetComponent<InputField>().text = "TwinName";  //resets the input Field 
             dataPersistenceManager.SaveConfig();
             fileManager.Refresh();
             InteractionController.EnableMode("Main");
         } else {
             Debug.Log("Twin-Name-Test of New-button push failed!");
+            //This is the new-Button because is create a completely new Twin
         }
         
     }
     
+    /*
+     * This methode is responsible, if the button safeAs is pressed in Twin-Mode.
+     * This Button creates a new Twin on the basis of another Twin.
+     */
     public void saveAsConfig()
     {
         string newTwinNameFromInput = GetTwinNameFromInput();
-        if (CheckInputTwinName(newTwinNameFromInput))
-        {
-            //Text origin = inputField.GetComponent<Text>();
+        // getting newest input from the InputField
+        if (CheckInputTwinName(newTwinNameFromInput)) {
+            // check if the new input is a already existing folder
             dataPersistenceManager.saveAsConfig(newTwinNameFromInput);
-            inputField.transform.Find("Text").GetComponent<Text>().text = "TwinName";
-            //textbox should be resetted
+            inputField.transform.GetComponent<InputField>().text = "TwinName";  //resets the input Field
             dataPersistenceManager.SaveConfig();
             fileManager.Refresh();
             InteractionController.EnableMode("Main");
         } else {
             Debug.Log("Twin-Name-Test of saveAs-button push failed!");
+            //This is the safeAs-Button because is create a completely new Twin
         }
     }
     
