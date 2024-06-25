1b71859d78ec1c7cf509acde1d1267d783143321
Torben <55281784+J0kxr@users.noreply.github.com>
2024-06-25 14:55:30 +0200

    Twin 91 twin name won t be checked if button is pressed (#85)
    * created acces to class TwinNameValidator for first Tests
    
    * outsurced the actucal conditon of twin name to methode
    
    * first tries to check twinname as button is pushed
    
    * added method that went lost while merging
    
    * name of twin should now be checked if button is pressed
    
    * refactored the code in 2 methods, new code currently only working while creating a new twin
    
    * included the whole input field from TwinMode into the config manager in the persistence manager
    
    * twin name check will be executed if buttons are pushed
    
    * input reference in saveAs-Method corrected
    
    * test for the name in input field
    
    * Refactored class commented
    
    ---------
    
    Co-authored-by: Torben Maieutec <torbenmaieutec@Air-von-Torben.fritz.box>
    Co-authored-by: Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.fritz.box>
    Co-authored-by: Torben Maieutec <torbenmaieutec@eduroam-141-23-214-27.wlan.tu-berlin.de>
    Co-authored-by: Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>

--- Maker/Assets/Code/ConfigManager.cs
+++ Maker/Assets/Code/ConfigManager.cs
@@ -8,27 +8,49 @@ public class ConfigManager : MonoBehaviour
 
     [SerializeField] private DataPersistenceManager dataPersistenceManager;
     [SerializeField] private FileManager fileManager;
-    [SerializeField] private GameObject newNameOrigin;
+    [SerializeField] private GameObject inputField;
+
+    /*
+     * This methode is responsible, if the button newConfig is pressed in Twin Mode.
+     * This Button creates a complete new Twin
+     */
     public void newConfig()
     {
-        Text origin = newNameOrigin.GetComponent<Text>();
-        dataPersistenceManager.createNewConfig(origin.text);
-        origin.transform.parent.gameObject.GetComponent<InputField>().text = "TwinName";
-        //textbox should be cleared
-        dataPersistenceManager.SaveConfig();
-        fileManager.Refresh();
-        InteractionController.EnableMode("Main");
+        string newTwinNameFromInput = GetTwinNameFromInput();
+        // getting newest input from the InputField
+        if (CheckInputTwinName(newTwinNameFromInput)) { 
+            // check if the new input is a already existing folder
+            dataPersistenceManager.createNewConfig(newTwinNameFromInput);      
+            inputField.transform.GetComponent<InputField>().text = "TwinName";  //resets the input Field 
+            dataPersistenceManager.SaveConfig();
+            fileManager.Refresh();
+            InteractionController.EnableMode("Main");
+        } else {
+            Debug.Log("Twin-Name-Test of New-button push failed!");
+            //This is the new-Button because is create a completely new Twin
+        }
+        
     }
     
+    /*
+     * This methode is responsible, if the button safeAs is pressed in Twin-Mode.
+     * This Button creates a new Twin on the basis of another Twin.
+     */
     public void saveAsConfig()
     {
-        Text origin = newNameOrigin.GetComponent<Text>();
-        dataPersistenceManager.saveAsConfig(origin.text);
-        origin.transform.parent.gameObject.GetComponent<InputField>().text = "TwinName";
-        //textbox should be cleared
-        dataPersistenceManager.SaveConfig();
-        fileManager.Refresh();
-        InteractionController.EnableMode("Main");
+        string newTwinNameFromInput = GetTwinNameFromInput();
+        // getting newest input from the InputField
+        if (CheckInputTwinName(newTwinNameFromInput)) {
+            // check if the new input is a already existing folder
+            dataPersistenceManager.saveAsConfig(newTwinNameFromInput);
+            inputField.transform.GetComponent<InputField>().text = "TwinName";  //resets the input Field
+            dataPersistenceManager.SaveConfig();
+            fileManager.Refresh();
+            InteractionController.EnableMode("Main");
+        } else {
+            Debug.Log("Twin-Name-Test of saveAs-button push failed!");
+            //This is the safeAs-Button because is create a completely new Twin
+        }
     }
     
     public void ResetApp()
@@ -36,6 +58,21 @@ public class ConfigManager : MonoBehaviour
         dataPersistenceManager.ResetApp();
         fileManager.Refresh();
         InteractionController.EnableMode("Main");
+    }
+
+    private string GetTwinNameFromInput() {
+        Transform Text = inputField.transform.Find("Text");
+        Text origin = Text.GetComponent<Text>();
+        Debug.Log("Text from Input: " + origin.text.ToString() + ".");//delete after Testing
+
+        return origin.text.ToString();
+    }
+
+    private bool CheckInputTwinName(string inputForTwinName) {
+        // Here I want to acces the the component from the input field
+        // this component twinNameValidator 
+        TwinNameValidator nameValidator = inputField.GetComponent<TwinNameValidator>();
+        return nameValidator.CheckInput(inputForTwinName);
     }
 
 }
