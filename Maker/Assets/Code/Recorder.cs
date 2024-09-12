b3a62a8bf02d759cd662177acc04bb6ab783272e
andreasschroff <andreas@schroff-online.com>
2024-09-12 12:46:41 +0200

    reeingineering recorder

--- Maker/Assets/Code/Recorder.cs
+++ Maker/Assets/Code/Recorder.cs
@@ -27,40 +27,10 @@ public class Recorder: MonoBehaviour
     private IEnumerator TakeAndSave(LeanPulse notification)
     {
         
-        Texture2D ss = new Texture2D(Screen.width, Screen.height-1200, TextureFormat.RGB24, false);
-        List<GameObject> ActiveChildren = new List<GameObject>();
-        for (int i = this.transform.childCount - 1; i >= 1; i--)
-        {
-            GameObject child = this.transform.GetChild(i).gameObject;
-            if (child.activeSelf)
-            {
-                ActiveChildren.Add(child);
-            }
-            child.SetActive(false);
-        }
+        List<GameObject> listActive = Prepare();
         yield return new WaitForEndOfFrame();
-        ss.ReadPixels(new Rect(0, 900, Screen.width, Screen.height-1200), 0, 0);
-        ss.Apply();
-        foreach (GameObject activeChild in ActiveChildren)
-        {
-            activeChild.SetActive(true);
-        }
-        string fullPath = get_path();
-        byte[] textureBytes = ss.EncodeToPNG();
-        File.WriteAllBytes(fullPath, textureBytes);
-        NativeGallery.Permission permission = NativeGallery.SaveImageToGallery(textureBytes, "twinAlbum", "screenshot_" + name + ".png", ( success, path ) => Debug.Log( "Media save result: " + success + " " + path ) );
-             
-#if UNITY_EDITOR
-        string message = "Screenshot saved  in application data under ..." + Path.Combine(
-            name, "screenshot_" + name + ".png");
-#else
-			string message = "Screenshot saved in Pictures as " + "screenshot_" + name + ".png";
-#endif
-        foreach (Text text in notification.gameObject.GetComponentsInChildren<Text>())
-        {
-            text.text = message;
-        }
-        notification.Pulse();
+        Do();
+        Post(listActive, notification);
         
     }
     
