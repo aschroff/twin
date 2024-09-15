8e009ac122c7baf5a69194b70f764b6dff883726
andreasschroff <andreas@schroff-online.com>
2024-09-15 12:15:16 +0200

    tour and screenshot works

--- Maker/Assets/Code/TourProcess.cs
+++ Maker/Assets/Code/TourProcess.cs
@@ -21,20 +21,17 @@ namespace Code
             SceneManagement.View currentView = viewManager.shootView();
             Recorder recorder = getRecorder();
             DataPersistenceManager dataManager = getDataManager();
-            List<GameObject> listActive = new List<GameObject>();
+            List<GameObject> listActive = recorder.Prepare();
             foreach (SceneManagement.View view in getStandardViewManager().views)
             {
                 viewManager.select(view);
                 recorder.name = dataManager.selectedProfileId + " - " + view.name;
                 recorder.folder = dataManager.selectedProfileId;
-                listActive = recorder.Prepare();
                 yield return new WaitForEndOfFrame();
                 recorder.Do();
             }
             recorder.Reset(listActive);
-            yield return new WaitForEndOfFrame();
             recorder.Post(getNotification());
-            yield return new WaitForEndOfFrame();
             viewManager.select(currentView);
             
         }
