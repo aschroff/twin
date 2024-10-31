8c0cf2d5ec90410f89b1790ec297bb8b4a3ad3fb
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2024-10-31 15:11:32 +0100

    twin name and twin version is now checked separately with error message in toast

--- Maker/Assets/Code/TwinNameValidator.cs
+++ Maker/Assets/Code/TwinNameValidator.cs
@@ -14,6 +14,9 @@ public class TwinNameValidator : MonoBehaviour
     private static readonly string nameRegex = "^[a-zA-Z0-9_()-]{1,11}$";
     //Regex which only allows string that are made out of the characters a-z, A-Z, 0-9 and "_", "(", ")","-"
     //with the minimum length of 1 and the maximum length of 14
+    private static readonly string invalidNameMessge = "Twin name can only contain following characters: a-z, A-Z, 0-9 and \"_\", \"(\", \")\",\" - \". With length between 1 and 14.";
+    private static readonly string alreadyExistingNameMessage = "This Twin Name already exists.";
+
 
 
     private void Start()
@@ -21,36 +24,52 @@ public class TwinNameValidator : MonoBehaviour
         nameInputField = this.GetComponent<InputField>();
     }
 
-    public bool validInput(string twinNameWithVersion) {
-        if (twinNameWithVersion.Count(t => t == '.') != 1) {
+    public bool validInput(string twinNameWithVersion)
+    {
+        if (twinNameWithVersion.Count(t => t == '.') != 1)
+        {
             //there has to be exactly one dot in the name - between the twinName and its version
-            DisplayErrorMessage("Twin name can only contain following characters: a-z, A-Z, 0-9 and \"_\", \"(\", \")\",\" - \". With length between 1 and 14.");
+            DisplayMessage(invalidNameMessge);
             return false;
         }
         string[] twinName = twinNameWithVersion.Split('.');
 
-        if (dataPersistenceManager.ExistsProfileId(twinName[0]))
-            {
-                //if there is already a twin with this name - the version of this twin should be checked
-                if (twinName[1] != "000")
-                {
-                    DisplayErrorMessage("Twin name can only contain following characters: a-z, A-Z, 0-9 and \"_\", \"(\", \")\",\" - \". With length between 1 and 14.");
-                    return false;
-                } else {
-                //if twinName already exists we know that this twins name is valid
-                    return true;
-                }
-            }
-            else {
+        if (dataPersistenceManager.ExistsProfileId(/*twinName[0]*/twinNameWithVersion))
+        {
+            //if there is already a twin with this name - the version of this twin should be checked
+            //it could be the case that we have only a new version of an already existing twin
+            return IsValidVersion(twinName[1]);
+        }
+        else
+        {
             //if twin Name did not already exists it has to be checked!
             if (!CheckInput(twinName[0]))
             {
-                DisplayErrorMessage("Twin name can only contain following characters: a-z, A-Z, 0-9 and \"_\", \"(\", \")\",\" - \". With length between 1 and 14.");
+                DisplayMessage(invalidNameMessge);
                 return false;
             }
-            else {
+            else
+            {
                 return true;
             }
+        }
+    }
+
+    private bool IsValidVersion(string twinName)
+    {
+        if (twinName.Equals("000"))
+        {
+            // the Version is 000 so there was an attempt to create a twin with a name that already exists or the version was typed in manually which is
+            // not allowed so nothing happens
+            DisplayMessage(alreadyExistingNameMessage);
+            return false;
+        }
+        else
+        {
+            //if the version is unlike 000 it had to be altered by the user or the code so is should be correct? (set criteria for version input)
+
+            //!!!insert here actual check for valid version!!!
+            return true;
         }
     }
 
@@ -67,7 +86,7 @@ public class TwinNameValidator : MonoBehaviour
         // regex is white page regex so it accepts exactly when the name meets the naming conditions
     }
 
-    private void DisplayErrorMessage(string errorMessage) {
+    private void DisplayMessage(string errorMessage) {
         string message = errorMessage;
 
         foreach (Text text in notification.gameObject.GetComponentsInChildren<Text>())
