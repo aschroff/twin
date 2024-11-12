1b7b595965831151f1747710163b43642933398a
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2024-11-12 15:24:50 +0100

    date is written into input field of twin versionUI

--- Maker/Assets/Code/TwinVersionSetter.cs
+++ Maker/Assets/Code/TwinVersionSetter.cs
@@ -1,12 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class TwinVersionSetter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
+        
+        InputField versionInput = this.GetComponent<InputField>();
+        versionInput.text = DateTime.Now.ToString("MM/dd/yyyy");
+
         
     }
 
