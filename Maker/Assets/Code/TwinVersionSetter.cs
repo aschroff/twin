e58607fc40a8a3a6d28dc7baa82053ef92f361a5
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2024-11-19 12:02:52 +0100

    first draft of getting date format from localization table

--- Maker/Assets/Code/TwinVersionSetter.cs
+++ Maker/Assets/Code/TwinVersionSetter.cs
@@ -3,17 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using System;
+using UnityEngine.Localization;
+using UnityEngine.Localization.Components;
+using UnityEngine.Localization.Tables;
+using UnityEngine.Localization.Settings;
 
 public class TwinVersionSetter : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-        
-        InputField versionInput = this.GetComponent<InputField>();
-        versionInput.text = DateTime.Now.ToString("MM/dd/yyyy");
+        LocalizeStringEvent localizeStringEvent = this.transform.GetComponentInChildren<LocalizeStringEvent>();
+        TableReference localizationTableReference = localizeStringEvent.StringReference.TableReference;
+        TableEntryReference localizationTableEntryReference = localizeStringEvent.StringReference.TableEntryReference;
 
-        
+        string currentDateFormat = LocalizationSettings.StringDatabase.
+            GetLocalizedString(localizationTableReference, localizationTableEntryReference);
+
+        //string currentDateFormat = localizeStringEvent.
+        //LocalizationSettings.StringDatabase.GetLocalizedString(tableReference, entryReference);
+        //if (localizeStringEvent.StringReference) {
+        //} else if () {
+
+        //} else if () {
+        //}
+        InputField versionInput = this.GetComponent<InputField>();
+        versionInput.text = currentDateFormat.Replace(".","-");
+        Debug.Log(versionInput.text);
+        //versionInput.text = DateTime.Now.ToString(currentDateFormat
+        //    .Replace(".","-")
+        //    .Substring(currentDateFormat.IndexOf(": ")));
+
+
     }
 
     // Update is called once per frame
