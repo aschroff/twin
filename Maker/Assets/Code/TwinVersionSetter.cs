8cb93a716d5bdf5dfeaed82f7730a741d47b0100
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2024-12-17 10:32:52 +0100

    added functionality to access last valid Date in TwinVersinSetter

--- Maker/Assets/Code/TwinVersionSetter.cs
+++ Maker/Assets/Code/TwinVersionSetter.cs
@@ -10,6 +10,7 @@ public class TwinVersionSetter : MonoBehaviour
     [SerializeField] private LocalizedString dateFormat;
     [SerializeField] private InputField inputFieldText;
     private LocalizedString formattedLocalizedString;
+    private string lastFormattedDate;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,22 +20,27 @@ public class TwinVersionSetter : MonoBehaviour
         // format the date to current localization settings
         formattedLocalizedString = DateFormatter.formatDate(currentDate, dateFormat);
 
-        //UpdateDate gets called, when event StringChanged is triggered
-        //parameter value of UpdateString is formatted date-string
+        // Subscribe to the StringChanged event
         formattedLocalizedString.StringChanged += UpdateDate;
-        //Triggers display update
+        // Triggers display update
         formattedLocalizedString.RefreshString();
     }
 
+    // Event handler method
     private void UpdateDate(string value)
     {
-        //InputField version = this.GetComponent<InputField>();
-        //Update UI with formatted date
+        // Update UI with formatted date
         inputFieldText.text = value;
+        this.lastFormattedDate = value;
     }
 
     private void OnDisable()
     {
         formattedLocalizedString.StringChanged -= UpdateDate;
+    }
+
+    public string GetLastFormattedDate()
+    {
+        return this.lastFormattedDate;
     }
 }
