08073ace27cbf41dd07dc84a6143565d7d78a062
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2025-01-22 17:16:41 +0100

    first draft of storing language in config

--- Maker/Assets/Code/Localization/LanguageSelector.cs
+++ Maker/Assets/Code/Localization/LanguageSelector.cs
@@ -4,9 +4,17 @@ using UnityEngine;
 
 using UnityEngine.Localization.Settings;
 
-public class LanguageSelector : MonoBehaviour
+
+public class LanguageSelector : MonoBehaviour, IDataPersistence   
 {
+    [SerializeField] public bool persistent = true;
     private bool active = false; //makes sure that coroutine is not called more than once
+    private int languageID;
+
+    public GameObject relatedGameObject()
+    {
+        return this.gameObject;
+    }
 
     public void ChangeLocale(int localeID)
     {
@@ -22,7 +30,29 @@ public class LanguageSelector : MonoBehaviour
         active = true;
         yield return LocalizationSettings.InitializationOperation; //makes sure that localization is loaded and ready to use
         LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
+        languageID = localeID;
         active = false;
 
+    }
+    
+    public void LoadData(ConfigData data)
+    {
+        if (persistent == false) return;
+        languageID = data.languageID;
+        if (languageID == null)
+        {
+            languageID = 0;
+        }
+        StartCoroutine(SetLocale(languageID));//error here because SettingManager is inactiv!
+    }
+
+    public void SaveData(ConfigData data)
+    {
+        if (persistent == false) return;
+        if (languageID == null)
+        {
+            languageID = 0;
+        }
+        data.languageID = languageID;
     }
 }
