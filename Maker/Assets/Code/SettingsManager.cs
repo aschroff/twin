b4212347c87248a640ef9a6bb9ccb168aa1a04c2
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2025-01-22 17:24:31 +0100

    calls changeLocale function in LanguageSelector

--- Maker/Assets/Code/SettingsManager.cs
+++ Maker/Assets/Code/SettingsManager.cs
@@ -8,7 +8,12 @@ public class SettingsManager : MonoBehaviour
 {
     [SerializeField] private DataPersistenceManager dataPersistenceManager;
     [SerializeField] private TMP_Dropdown dropdown;
-    private bool active = false; //makes sure that coroutine is not called more than once
+    private LanguageSelector languageSelector;
+
+    void Start()
+    {
+        Debug.Log("Settings Manager started!");
+    }
 
     public void ResetApp()
     {
@@ -19,24 +24,8 @@ public class SettingsManager : MonoBehaviour
     public void GetSelectedLanguage() {
         //localeID is SetFontSize bu order of the languages  in the localization table
         // if options in drop down menu are in the same order as the languages in the localization table, we can directly parse the dopdown value as the localeID
-        ChangeLocale(dropdown.value);
-    }
 
-    public void ChangeLocale(int localeID)
-    {
-        if (active == true)
-        {
-            return;
-        }
-        StartCoroutine(SetLocale(localeID));
-    }
-
-    IEnumerator SetLocale(int localeID)
-    {
-        active = true;
-        yield return LocalizationSettings.InitializationOperation; //makes sure that localization is loaded and ready to use
-        LocalizationSettings.SelectedLocale = LocalizationSettings.AvailableLocales.Locales[localeID];
-        active = false;
-
+        languageSelector = this.gameObject.GetComponent<LanguageSelector>();
+        languageSelector.ChangeLocale(dropdown.value);
     }
 }
