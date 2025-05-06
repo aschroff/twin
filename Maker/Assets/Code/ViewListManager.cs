994cdb7b938366a9bc309aa5b38c70dbe49e8b6a
Torben Maieutec <torbenmaieutec@MacBook-Air-von-Torben.local>
2025-05-06 10:40:14 +0200

    added List of Views into ViewListUI

--- Maker/Assets/Code/ViewListManager.cs
+++ Maker/Assets/Code/ViewListManager.cs
@@ -1,39 +1,198 @@
 using UnityEngine;
-using Lean.Gui;
+using System.Linq;
+using Lean.Common;
+using Lean.Touch;
 using UnityEngine.UI;
 
-public class ViewListManager : MonoBehaviour, ItemFile
+public class ViewListManager : SceneManagement, IDataPersistence
 {
-    [SerializeField] private ViewManager viewmanager;
     [SerializeField] private GameObject prefab;
-    [SerializeField] private LeanPulse notification;
-    public void handleChange(string profile)
+    [SerializeField] private ViewManager viewManager;
+    [SerializeField] private PartManager partManager;
+
+    private bool cameraEnabled = true;
+
+    private void Start()
     {
-        throw new System.NotImplementedException();
+        if (partManager == null)
+        {
+            partManager = this.transform.parent.parent.parent.GetComponentInParent<PartManager>();
+        }
+        //Uncomment this if-Block if the view Manager in Unity wont be hold as it happens with the partManager
+        //if (viewManager == null)
+        //{
+        //    viewManager = this.transform.parent.parent.parent.GetComponentInParent<ViewManager>();
+        //}
+
     }
 
-    public void handleCopyChange(string profile)
+    public void build()
     {
-        throw new System.NotImplementedException();
+        foreach (View view in views)
+        {
+            displayView(view);
+        }
     }
 
-    public void handleDelete(string profile)
+    public void clear()
     {
-        throw new System.NotImplementedException();
+        for (int j = 0; j < this.transform.childCount; j++)
+        {
+            GameObject child = this.transform.GetChild(j).gameObject;
+            Destroy(child);
+        }
+    }
+
+    public void rebuild()
+    {
+        clear();
+        build();
+    }
+
+    public void displayView(View view)
+    {
+        GameObject viewObject = Instantiate(prefab);
+        viewObject.transform.SetParent(this.transform, false);
+        viewObject.transform.localScale = prefab.transform.localScale;
+        viewObject.GetComponent<ViewLink>().link = view;
+        viewObject.GetComponent<ViewLink>().manager = viewManager;
+        viewObject.GetComponentInChildren<InputField>().text = view.name;
+    }
+    public View shootView()
+    {
+        View view = new View();
+        LeanPitchYaw control = body.GetComponent<LeanPitchYaw>();
+        view.yaw = control.Yaw;
+        view.pitch = control.Pitch;
+        view.positionCamera_x = mainCamera.transform.position.x;
+        view.positionCamera_y = mainCamera.transform.position.y;
+        view.positionCamera_z = mainCamera.transform.position.z;
+        LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+        view.sizeCamera = camera.Zoom;
+        view.initial = false;
+        return view;
+    }
+
+    public void createView()
+    {
+        View view = shootView();
+        views.Add(view);
+        displayView(view);
+    }
+
+    public void LoadData(ConfigData data)
+    {
+
+        var json = data.views;
+        if (json == "")
+        {
+            views.Clear();
+        }
+        else
+        {
+            GameObject prefab_save = prefab;
+            GameObject mainCamera_save = this.mainCamera;
+            GameObject body_save = body;
+            PartManager partManagerTemp = partManager;
+            //Uncomment the two comments if the view Manager in Unity wont be hold as it happens with the partManager
+            //ViewManager viewMangerTemp = viewManager;
+
+            JsonUtility.FromJsonOverwrite(json, this);
+
+            prefab = prefab_save;
+            mainCamera = mainCamera_save;
+            body = body_save;
+            partManager = partManagerTemp;
+            //viewManager = viewMangerTemp;
+        }
+
+        rebuild();
+    }
+
+    public void SaveData(ConfigData data)
+    {
+        data.views = JsonUtility.ToJson(this);
+    }
+
+    public GameObject relatedGameObject()
+    {
+        return this.gameObject;
     }
 
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void delete(View view)
     {
-        
+        views.Remove(view);
+        rebuild();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void select(View view)
     {
-        
+        partManager.EnforceNewPart();
+        LeanPitchYaw control = body.GetComponent<LeanPitchYaw>();
+        control.Yaw = view.yaw;
+        control.Pitch = view.pitch;
+        mainCamera.transform.position = new Vector3(view.positionCamera_x, view.positionCamera_y, view.positionCamera_z);
+        LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+        cameraEnabled = camera.enabled;
+        camera.enabled = true;
+        camera.Zoom = view.sizeCamera;
     }
 
-    //Copy from Group
+    public void setDefaultPosition()
+    {
+        partManager.EnforceNewPart();
+        LeanPitchYaw control = body.GetComponent<LeanPitchYaw>();
+        control.Yaw = 0.0f;
+        control.Pitch = 0.0f;
+        mainCamera.transform.position = new Vector3(x: 0.0f, y: 0.5f, z: 1.0f);
+        LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+        camera.Zoom = 2.0f;
+    }
+
+    void LateUpdate()
+    {
+        LeanPinchCamera camera = mainCamera.GetComponent<LeanPinchCamera>();
+        camera.enabled = cameraEnabled;
+    }
+
+
+    public void Show(bool visible)
+    {
+        RectTransform recttransform = this.gameObject.transform.parent.transform.parent.GetComponent<RectTransform>();
+        float width = recttransform.rect.width;
+        if (visible & recttransform.anchoredPosition.x <= 0)
+        {
+
+        }
+        else if (!visible & recttransform.anchoredPosition.x <= 0)
+        {
+            recttransform.anchoredPosition += new Vector2(width, 0);
+        }
+        else if (!visible & recttransform.anchoredPosition.x > 0)
+        {
+
+        }
+        else if (visible & recttransform.anchoredPosition.x > 0)
+        {
+            recttransform.anchoredPosition += new Vector2(-width, 0);
+        }
+    }
+
+    public void toggleShow()
+    {
+        RectTransform recttransform = this.gameObject.transform.parent.transform.parent.GetComponent<RectTransform>();
+        bool newVisible = (recttransform.anchoredPosition.x > 0);
+        Show(newVisible);
+    }
+
+    public void AddList()
+    {
+        StandardViewManager standards = this.transform.parent.transform.parent.GetComponent<StandardViewManager>();
+        views.AddRange(standards.views);
+        rebuild();
+    }
+
 }
+
