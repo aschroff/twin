bfc2dd2a4ceb3252c5f03c4ed42849d4bafe6aa9
andreasschroff <andreas@schroff-online.com>
2025-06-22 17:00:02 +0200

    TWIN-370 Added Finalize Process and added process gameobjects

---
+++ Maker/Assets/Code/Processes/Meshcapade/FinalizeMeshcapadeImageProcess.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Code.Processes.Meshcapade
+{
+
+    
+    public class FinalitzeMeshcapadeImageProcess : ProcessSync
+    {
+        [SerializeField] public string apiPath;
+        
+        public override ProcessResult ExecuteSync(string variant = "", ProcessResult previousResult = null)
+        {
+            return new ProcessResult();
+        }
+        public override ProcessResult Execute(string variant = "")
+        {
+            return ExecuteSync(variant);
+        } 
+        
+    }
+}
