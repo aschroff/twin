08ea39c3d237eea87190858e151e796e8321ec43
Ella Maieutec <ellamaieutec@MacBook-Air-von-Ella.local>
2025-07-25 13:17:44 +0200

    changed Text component search to TMP GUI, replaced 'no group' to '-'

--- Maker/Assets/Code/GroupDisplay.cs
+++ Maker/Assets/Code/GroupDisplay.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 public class GroupDisplay : MonoBehaviour 
@@ -5,14 +6,14 @@ public class GroupDisplay : MonoBehaviour
     [SerializeField] private PartManager partmanager; 
     private void Update()
     {
-        Text currentGroupText = this.gameObject.GetComponent<Text>();
+        TextMeshProUGUI currentGroupText = this.gameObject.GetComponent<TextMeshProUGUI>();
         if (partmanager.currentGroup != null && partmanager.currentGroup.group != null )
         {
             currentGroupText.text = partmanager.currentGroup.name;  
         }
         else
         {
-            currentGroupText.text = "<no group>";
+            currentGroupText.text = "-";
         }
         
     }
