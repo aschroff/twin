1c17122e58e15fccbada73d3e33f5479f7c153a2
Ella Maieutec <ellamaieutec@MacBook-Air-von-Ella.fritz.box>
2026-04-28 14:56:29 +0200

    storing part description on disable and application quit

--- Maker/Assets/Code/View/Manager/PartDetailManager.cs
+++ Maker/Assets/Code/View/Manager/PartDetailManager.cs
@@ -18,6 +18,16 @@ namespace Code
             Display();
         }
 
+        void OnDisable()
+        {
+            storeDataDescription();
+        }
+
+        void OnApplicationQuit()
+        {
+            storeDataDescription();
+        }
+
     
         // Start is called before the first frame update
         void Start()
@@ -59,6 +69,15 @@ namespace Code
             }
             
             
+        }
+
+        void storeDataDescription()
+        {
+            InputField input_field = this.transform.GetComponentInChildren<InputField>();
+            if(input_field != null)
+            {
+                InteractionController.Partdata.description = input_field.text;
+            }
         }
     }
 }
